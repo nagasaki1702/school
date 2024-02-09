97a19e714571ca93fdaf2dfcb6b4395d02fc9295
nagasaki1702 <102396852+nagasaki1702@users.noreply.github.com>
2024-02-09 10:24:21 +0900

    ブログ一覧にカテゴリー表示させるように修正

--- resources/views/posts/partials/categories-box.blade.php
+++ resources/views/posts/partials/categories-box.blade.php
@@ -5,7 +5,6 @@
         @foreach ($categories as $category) 
         <x-badge :textColor="$category->text_color" :bgColor="$category->bg_color">
         {{-- <x-badge textColor="red" bgColor="red"> --}}
-
             {{ $category->title }}
         </x-badge>
         @endforeach
